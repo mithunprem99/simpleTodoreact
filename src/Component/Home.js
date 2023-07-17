0d5dbad821dd84ba041b4e021f5a5d78abd08732
mithunprem99 <mithunprem1234@gmail.com>
2023-07-17 16:09:18 +0530

    added refetch to fetch the api

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -3,12 +3,17 @@ import  axios  from 'axios'
 import React from 'react'
 
 export const Home = () => {
-  const { data } = useQuery(['cat'],()=>{
+  const { data,isLoading,refetch } = useQuery(['cat'],()=>{
     return axios.get("https://catfact.ninja/fact").then((resp)=>resp.data)
     
    
   })
+  if(isLoading){
+    return <h1>Loading...</h1>
+  }
   return (
-    <div>this is home{data?.fact}</div>
+    <div>this is home{data?.fact}
+    <button onClick={refetch}>update</button>
+    </div> 
   )
 }
